46e5a6f63978579269b1f4bbd0cc56b2453c85aa
Scott Nonnenberg <scott@nonnenberg.com>
2016-06-03 11:02:09 -0700

    Export all five functions

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 /* eslint-disable global-require */
 
 module.exports = {
+  getConfig: require('./src/get_config'),
   getDifferences: require('./src/get_differences'),
+  getLiteralConfig: require('./src/get_literal_config'),
+  getScore: require('./src/get_score'),
   renderDifferences: require('./src/render_differences'),
 };
--- test/unit/test_index.js
+++ test/unit/test_index.js
@@ -8,8 +8,20 @@ var index = require('index');
 
 
 describe('unit/index', function() {
+  it('has truthy getConfig key', function() {
+    expect(index).to.have.property('getConfig').that.exist;
+  });
+
   it('has truthy getDifferences key', function() {
     expect(index).to.have.property('getDifferences').that.exist;
+  });
+
+  it('has truthy getLiteralConfig key', function() {
+    expect(index).to.have.property('getLiteralConfig').that.exist;
+  });
+
+  it('has truthy getScore key', function() {
+    expect(index).to.have.property('getScore').that.exist;
   });
 
   it('has truthy renderDifferences key', function() {
@@ -17,6 +29,6 @@ describe('unit/index', function() {
   });
 
   it('has two keys', function() {
-    expect(_.keys(index)).to.have.length(2);
+    expect(_.keys(index)).to.have.length(5);
   });
 });
