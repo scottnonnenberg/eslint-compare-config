4a47c913b6211bb7fb153a5eb68533426f8c47b6
Scott Nonnenberg <scott@nonnenberg.com>
2016-06-03 15:15:02 -0700

    Add tests for two-arg CLI requirement

--- test/integration/test_cli.js
+++ test/integration/test_cli.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var chai = require('chai');
+var expect = chai.expect;
+
 var cli = require('src/cli');
 
 
@@ -7,23 +10,37 @@ describe('integration/cli', function() {
   var left = 'test/integration/left/.eslintrc.js';
   var right = 'test/integration/right/.eslintrc.js';
 
+  it('throws if only one arg provided', function() {
+    expect(function() {
+      cli(['node', 'boot.js', left]);
+    }).to.throw(Error)
+      .that.match(/needs two configuration/);
+  });
+
+  it('throws if three args provided', function() {
+    expect(function() {
+      cli(['node', 'boot.js', left, right, right]);
+    }).to.throw(Error)
+      .that.match(/needs two configuration/);
+  });
+
   it('shows help', function() {
-    cli(['node', 'file.js', left, right, '--help']);
+    cli(['node', 'boot.js', left, right, '--help']);
   });
 
   it('shows differences between two files', function() {
-    cli(['node', 'file.js', left, right]);
+    cli(['node', 'boot.js', left, right]);
   });
 
   it('shows literal differences between two files', function() {
-    cli(['node', 'file.js', left, right, '--literal']);
+    cli(['node', 'boot.js', left, right, '--literal']);
   });
 
   it('shows json differences', function() {
-    cli(['node', 'file.js', left, right, '--json']);
+    cli(['node', 'boot.js', left, right, '--json']);
   });
 
   it('shows score', function() {
-    cli(['node', 'file.js', left, right, '--score']);
+    cli(['node', 'boot.js', left, right, '--score']);
   });
 });
