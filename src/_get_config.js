f23fdc4107c10ea6a8a7dcc8b19dcbd2153b9e04
Scott Nonnenberg <scott@nonnenberg.com>
2021-12-07 21:33:16 -0800

    feat(eslint): Support for modern eslint versions

--- src/_get_config.js
+++ src/_get_config.js
@@ -5,10 +5,16 @@
 var path = require('path');
 var eslint = require('eslint');
 
-var CLIEngine = eslint.CLIEngine;
+var ESLint = eslint.ESLint || eslint.CLIEngine;
 
-var engine = new CLIEngine();
+var engine = new ESLint();
 var target = path.join(__dirname, '__random.js');
-var config = engine.getConfigForFile(target);
 
-console.log(JSON.stringify(config, null, '  '));
+if (engine.calculateConfigForFile) {
+  engine.calculateConfigForFile(target).then(function(config) {
+    console.log(JSON.stringify(config, null, '  '));
+  });
+} else {
+  var config = engine.getConfigForFile(target);
+  console.log(JSON.stringify(config, null, '  '));
+}
